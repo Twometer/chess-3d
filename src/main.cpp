e1fde11fb346b05ebcffba40448ba3a1e16457ad
Twometer <twometer@outlook.de>
2019-12-13 15:54:17 +0100

    We have a window now

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "model/Board.h"
-#include "model/Rule.h"
 #include "model/Ruleset.h"
+
+using namespace std;
 
 int main() {
     Ruleset *ruleset = Ruleset::Load("assets/rules.json");
@@ -11,6 +13,34 @@ int main() {
 
     if (!glfwInit())
         return 1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "3D Chess", nullptr, nullptr);
+    if (!window) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return 1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        cout << "Failed to initialize OpenGL" << endl;
+        glfwTerminate();
+        return 1;
+    }
+
+    cout << "Successfully initialized" << endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
 
     return 0;
 }
